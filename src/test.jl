c526ffce2a1c673113c7587594626b87f034db0f
Nicolas Brantut <nicolas.brantut@normalesup.org>
2020-05-24 18:09:19 +0200

    update test with vsv

--- src/test.jl
+++ src/test.jl
@@ -9,13 +9,13 @@ include("invert.jl")
 println("Running test...")
 println("Make synthetic data...")
 
-m0 = [2,1.2,0.1,0.15,0.03,deg2rad(25), deg2rad(40), deg2rad(55), deg2rad(90),deg2rad(90)]
+m0 = [2,1.2,0.1,0.15,0.03,deg2rad(25), deg2rad(40), deg2rad(55), deg2rad(90),deg2rad(90),deg2rad(90)]
 ρ = 1.
 
 C0 = thomsen2moduli(m0[1],m0[2],ρ,m0[3],m0[4],m0[5])
 
 np=4
-nsv=0
+nsv=1
 nsh=1
 
 args = (np, nsv, nsh)
@@ -23,17 +23,18 @@ args = (np, nsv, nsh)
 dobs = _gt(m0, np, nsv, nsh)
 
 p = [VMeasure(dobs[2k-1],exp(dobs[2k]),.01,0.01) for k in 1:np]
-sh = [VMeasure(dobs[2np+2k-1],exp(dobs[2np+2k]),.01,0.01) for k in 1:nsh]
+sv = [VMeasure(dobs[2np+2k-1],exp(dobs[2np+2k]),.01,0.01) for k in 1:nsv]
+sh = [VMeasure(dobs[2np+2nsv+2k-1],exp(dobs[2np+2nsv+2k]),.01,0.01) for k in 1:nsh]
 
 # a prior moel parameters
-m1 = [2.2,1.3,0.0,0.0,0.,deg2rad(25), deg2rad(40), deg2rad(55), deg2rad(90),deg2rad(90)]
+m1 = [2.2,1.5,0.0,0.0,0.,deg2rad(25), deg2rad(40), deg2rad(55), deg2rad(90),deg2rad(90)]
 
 C1 = thomsen2moduli(m1[1],m1[2],ρ,m1[3],m1[4],m1[5])
 
 # run inversion thomsen
 println("Run inversion to find Thomsen parameters...")
 
-m, anglep, anglesv, anglesh, cm = findthomsen(p,VMeasure[],sh, m1[1], m1[2], m1[3], m1[4], m1[5])
+m, anglep, anglesv, anglesh, cm = findthomsen(p,sv,sh, m1[1], m1[2], m1[3], m1[4], m1[5])
 
 println()
 println("Model parameters: input | inverted")
@@ -48,7 +49,7 @@ println()
 # run inversion moduli
 println("Run inversion to find moduli...")
 
-C, anglep, anglesv, anglesh, cm = findmoduli(p,VMeasure[],sh, ρ, C1[1,1], C1[1,3], C1[3,3], C1[5,5], C1[6,6], 100)
+C, anglep, anglesv, anglesh, cm = findmoduli(p,sv,sh, ρ, C1[1,1], C1[1,3], C1[3,3], C1[5,5], C1[6,6], 100)
 
 m = vcat([C[1,1], C[1,3], C[3,3], C[5,5], C[6,6]], anglep, anglesv, anglesh)
 
