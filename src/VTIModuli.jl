306a96521616ac22292d5339f57d39c81747cf30
Nicolas Brantut <nicolas.brantut@normalesup.org>
2020-05-24 19:07:53 +0200

    add readme

---
+++ src/VTIModuli.jl
@@ -0,0 +1,15 @@
+module VTIModuli
+
+using NewtonMethods: quasinewton
+using LinearAlgebra: I, Diagonal, Symmetric
+using Roots: fzero
+
+export vphaseP, vphaseSV, vphaseSH, moduli2thomsen, velocities0, thomsen2moduli, vgroup, groupangle, dvPdthetav, dvSVdthetav, vgroupSH, groupangleSH, phaseangleP, phaseangleSV, phaseangleSH, vpvsYoungsPoisson, stiffmatrix
+export findthomsen, findmoduli
+export VMeasure
+
+include("base.jl")
+include("types.jl")
+include("invert.jl")
+
+end
