4949f1dd9b2f88a68c6b7f92424ef3c50286fd0e
Nicolas Brantut <nicolas.brantut@normalesup.org>
2020-11-26 16:00:09 +0100

    added crack density inversion

--- src/base.jl
+++ src/base.jl
@@ -1,3 +1,8 @@
+"""
+    vphaseP(θ, VP0, VS0, ϵ, δ)
+
+Compute P wave phase velocity along phase angle θ (with respect to axis of symmetry) based on Vp₀ and Vs₀ (P and S wave speed along axis of symmetry) and Thomsen parameters ϵ and δ.
+"""
 function vphaseP(θ,VP0,VS0,ϵ,δ)
     f = 1 - (VS0^2)/(VP0^2)
     D = sqrt(1 + 4*sin(θ)^2/f*(2*δ*cos(θ)^2 - ϵ*cos(2θ)) +
@@ -6,6 +11,11 @@ function vphaseP(θ,VP0,VS0,ϵ,δ)
     return sqrt(vsq)*VP0
 end
 
+"""
+    vphaseSV(θ, VP0, VS0, ϵ, δ)
+
+Compute SV wave phase velocity along phase angle θ (with respect to axis of symmetry) based on Vp₀ and Vs₀ (P and S wave speed along axis of symmetry) and Thomsen parameters ϵ and δ.
+"""
 function vphaseSV(θ,VP0,VS0,ϵ,δ)
     f = 1 - (VS0^2)/(VP0^2)
     D = sqrt(1 + 4*sin(θ)^2/f*(2*δ*cos(θ)^2 - ϵ*cos(2θ)) +
@@ -14,11 +24,21 @@ function vphaseSV(θ,VP0,VS0,ϵ,δ)
     return sqrt(vsq)*VP0
 end
 
+"""
+    vphaseSH(θ, VP0, VS0, γ)
+
+Compute SH wave phase velocity along phase angle θ (with respect to axis of symmetry) based on Vp₀ and Vs₀ (P and S wave speed along axis of symmetry) and Thomsen parameter γ.
+"""
 function vphaseSH(θ,VS0,γ)
     return VS0*sqrt(1 + 2*γ*sin(θ)^2)
 end
 
-function vphaseP(θ,c::AbstracMatrix,ρ)
+"""
+    vphaseP(θ, c::AbstractMatrix, ρ)
+
+Compute P wave phase velocity along phase angle θ (with respect to axis of symmetry) based on stiffness matrix c (in Voight notation) and density ρ.
+"""
+function vphaseP(θ,c::AbstractMatrix,ρ)
     stsq = sin(θ)^2
     ctsq = cos(θ)^2
     D = sqrt(((c[1,1]-c[5,5])*stsq - (c[3,3]-c[5,5])*ctsq)^2 +
@@ -27,7 +47,12 @@ function vphaseP(θ,c::AbstracMatrix,ρ)
     return sqrt(rv/(2ρ))
 end
 
-function vphaseSV(θ,c::Matrix,ρ)
+"""
+    vphaseSV(θ, c::AbstractMatrix, ρ)
+
+Compute SV wave phase velocity along phase angle θ (with respect to axis of symmetry) based on stiffness matrix c (in Voight notation) and density ρ.
+"""
+function vphaseSV(θ,c::AbstractMatrix,ρ)
     stsq = sin(θ)^2
     ctsq = cos(θ)^2
     D = sqrt(((c[1,1]-c[5,5])*stsq - (c[3,3]-c[5,5])*ctsq)^2 +
@@ -36,21 +61,41 @@ function vphaseSV(θ,c::Matrix,ρ)
     return sqrt(rv/(2ρ))
 end
 
-function vphaseSH(θ,c::AbstracMatrix,ρ)
+"""
+    vphaseSH(θ, c::AbstractMatrix, ρ)
+
+Compute SH wave phase velocity along phase angle θ (with respect to axis of symmetry) based on stiffness matrix c (in Voight notation) and density ρ.
+"""
+function vphaseSH(θ,c::AbstractMatrix,ρ)
     return sqrt((c[6,6]*sin(θ)^2 + c[5,5]*cos(θ)^2)/ρ)
 end
 
-function moduli2thomsen(c::AbstracMatrix)
+"""
+    moduli2thomsen(c::AbstractMatrix)
+
+Compute Thomsen's parameters (ϵ, δ, γ) based on stiffness matrix c (in Voight notation).
+"""
+function moduli2thomsen(c::AbstractMatrix)
     epsilon = (c[1,1] - c[3,3])/(2*c[3,3])
     delta = ((c[1,3]+c[5,5])^2 - (c[3,3]-c[5,5])^2)/(2*c[3,3]*(c[3,3]-c[5,5]))
     gamma = (c[6,6]-c[5,5])/(2*c[5,5])
     return epsilon, delta, gamma
 end
 
+"""
+    velocities0(c::AbstractMatrix, ρ)
+
+Compute phase velocities Vp₀ and Vs₀ (P and S wave speed along symmetry axis) based on stiffness matrix c (in Voight notation) and density ρ.
+"""
 function velocities0(c::AbstractMatrix,ρ)
     return (sqrt(c[3,3]/ρ), sqrt(c[5,5]/ρ))
 end
 
+"""
+    thomsen2moduli(VP0, VS0, ρ, ϵ, δ, γ)
+
+Compute 6×6 stiffness matrix from Vp₀, Vs₀ (P and S wave speed along symmetry axis), density ρ, and Thomsen parameters ϵ, δ and γ.
+"""
 function thomsen2moduli(VP0,VS0,ρ,ϵ,δ,γ)
     c33 = ρ*VP0^2
     c55 = ρ*VS0^2
@@ -61,6 +106,11 @@ function thomsen2moduli(VP0,VS0,ρ,ϵ,δ,γ)
     return stiffmatrixTI(c11,c13,c33,c55,c66)
 end
 
+"""
+    vpvsYoungsPoisson(vp,vs,ρ)
+
+Compute (E, ν), Youngs modulus and Poisson's ratio, from P and S wave speeds, assuming isotropy.
+"""
 function vpvsYoungsPoisson(vp,vs,ρ)
 
     nu = (0.5*(vp/vs)^2 - 1)/((vp/vs)^2 - 1)
@@ -69,6 +119,11 @@ function vpvsYoungsPoisson(vp,vs,ρ)
     return E, nu
 end
 
+"""
+    stiffmatrixTI(E, nu)
+
+Compute 6×6 stiffness matrix in an isotropic medium characterised by Young's modulus E and Poisson's ratio nu.
+"""
 function stiffmatrixTI(E,nu)
     c0 = E/(1+nu)/(1-2nu)
     c = zeros(typeof(E),6,6)
@@ -84,7 +139,11 @@ function stiffmatrixTI(E,nu)
     return Symmetric(c)
 end
 
+"""
+    stiffmatrixTI(c11, c13, c33, c55, c66)
 
+Compute 6×6 stiffness matrix in a VTI medium characterised by components (in Voight notation) c11, c13, c33, c55 and c66.
+"""
 function stiffmatrixTI(c11,c13,c33,c55,c66)
     c = zeros(typeof(c11),6,6)
     c[1,1] = c11
@@ -99,6 +158,11 @@ function stiffmatrixTI(c11,c13,c33,c55,c66)
     return Symmetric(c)
 end
 
+"""
+    dvPdthetav(θ, VP0VS0, ϵ, δ)
+
+Compute (1/v)∂v/∂θ, derivative of P wave phase velocity  with respect to phase angle θ, normalised by Vp, based on ratio Vp₀/Vs₀ (P and S wave speed along symmetry axis) and Thomsen parameters ϵ and δ.
+"""
 function dvPdthetav(θ,VP0VS0,ϵ,δ)
 
     f = 1 - VP0VS0^(-2)
@@ -110,6 +174,11 @@ function dvPdthetav(θ,VP0VS0,ϵ,δ)
     return vpv
 end
 
+"""
+    dvPdthetav(θ, VP0VS0, ϵ, δ)
+
+Compute (1/v)∂v/∂θ, derivative of SV wave phase velocity  with respect to phase angle θ, normalised by Vsv, based on ratio Vp₀/Vs₀ (P and S wave speed along symmetry axis) and Thomsen parameters ϵ and δ.
+"""
 function dvSVdthetav(θ,VP0VS0,ϵ,δ)
 
     f = 1 - VP0VS0^(-2)
@@ -121,30 +190,65 @@ function dvSVdthetav(θ,VP0VS0,ϵ,δ)
     return vpv
 end
 
+"""
+    vgroup(v,dvv)
+
+Compute group wave velocity based on phase velocity v and normalised derivative of phase velocity with respect to phase angle dvv.
+"""
 function vgroup(v,dvv)
     return v*sqrt(1 + dvv^2)
 end
 
+"""
+    groupangle(θ, dvdθv)
+
+Compute group angle from phase angle θ and normalised derivative of phase velocity with respect to phase angle.
+"""
 function groupangle(θ,dvdθv)
     return atan((sin(θ) + dvdθv*cos(θ))/(cos(θ) - dvdθv*sin(θ)))
 end
 
+"""
+    vgroupSH(ψ, VS0, γ)
+
+Compute SH group velocity along group angle ψ, based on Vs₀ (S wave speed along symmetry axis) and Thomsen parameter γ.
+"""
 function vgroupSH(ψ,VS0,γ)
     return VS0*sqrt((1+2*γ)/(1+2*γ*cos(ψ)^2))
 end
 
+"""
+    groupangleSH(θ,γ)
+
+Compute SH group angle based on phase angle θ and Thomsen parameter γ.
+"""
 function groupangleSH(θ,γ)
     return atan(tan(θ)*(1+2*γ))
 end
 
+"""
+    phaseangleP(ψ, VP0VS0, ϵ, δ)
+
+Determine (using root finding method) the P wave phase angle based on group angle ψ, ratio Vp₀/Vs₀ (P and S wave speed along symmetry axis), and Thomsen parameters ϵ and δ.
+"""
 function phaseangleP(ψ,VP0VS0,ϵ,δ)
     fzero(θ->(ψ - groupangle(θ,dvPdthetav(θ,VP0VS0,ϵ,δ))),ψ)
 end
 
+"""
+    phaseangleSV(ψ, VP0VS0, ϵ, δ)
+
+Determine (using root finding method) the SV wave phase angle based on group angle ψ, ratio Vp₀/Vs₀ (P and S wave speed along symmetry axis), and Thomsen parameters ϵ and δ.
+"""
 function phaseangleSV(ψ,VP0VS0,ϵ,δ)
     fzero(θ->(ψ - groupangle(θ,dvSVdthetav(θ,VP0VS0,ϵ,δ))),ψ)
 end
 
+"""
+    phaseangleSH(ψ, γ)
+
+Compute SH wave phase angle based on group angle ψ and Thomsen parameter γ.
+"""
 function phaseangleSH(ψ,γ)
     return atan(tan(ψ)/(1+2*γ))
 end
