175d20588a327a1f0b24b9955bc325c16e56cde0
Nicolas Brantut <nicolas.brantut@normalesup.org>
2021-11-06 18:13:47 +0000

    add cracks

--- src/VTIModuli.jl
+++ src/VTIModuli.jl
@@ -4,12 +4,13 @@ using NewtonMethods: quasinewton
 using LinearAlgebra: I, Diagonal, Symmetric
 using Roots: fzero
 
-export vphaseP, vphaseSV, vphaseSH, moduli2thomsen, velocities0, thomsen2moduli, vgroup, groupangle, dvPdthetav, dvSVdthetav, vgroupSH, groupangleSH, phaseangleP, phaseangleSV, phaseangleSH, vpvsYoungsPoisson, stiffmatrix
-export findthomsen, findmoduli
+export vphaseP, vphaseSV, vphaseSH, moduli2thomsen, velocities0, thomsen2moduli, vgroup, groupangle, dvPdthetav, dvSVdthetav, vgroupSH, groupangleSH, phaseangleP, phaseangleSV, phaseangleSH, vpvsYoungsPoisson, stiffmatrix, cracks2moduli
+export findthomsen, findmoduli, findcracks
 export VMeasure
 
 include("base.jl")
 include("types.jl")
 include("invert.jl")
+include("cracks.jl")
 
 end
