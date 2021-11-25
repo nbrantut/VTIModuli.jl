dae43767f58f60bb7a073853ba25699059f5bc0e
Nicolas Brantut <nicolas.brantut@normalesup.org>
2021-11-25 12:49:00 +0000

    fix export sitffness

--- src/VTIModuli.jl
+++ src/VTIModuli.jl
@@ -4,7 +4,7 @@ using NewtonMethods: quasinewton
 using LinearAlgebra: I, Diagonal, Symmetric
 using Roots: fzero
 
-export vphaseP, vphaseSV, vphaseSH, moduli2thomsen, velocities0, thomsen2moduli, vgroup, groupangle, dvPdthetav, dvSVdthetav, vgroupSH, groupangleSH, phaseangleP, phaseangleSV, phaseangleSH, vpvsYoungsPoisson, stiffmatrix, cracks2moduli
+export vphaseP, vphaseSV, vphaseSH, moduli2thomsen, velocities0, thomsen2moduli, vgroup, groupangle, dvPdthetav, dvSVdthetav, vgroupSH, groupangleSH, phaseangleP, phaseangleSV, phaseangleSH, vpvsYoungsPoisson, stiffmatrixTI, cracks2moduli
 export findthomsen, findmoduli, findcracks
 export VMeasure
 
